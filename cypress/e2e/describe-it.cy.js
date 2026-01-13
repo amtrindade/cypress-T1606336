0891f8d840ee1718631d84864a55c31700c69b3c
Antonio Trindade <atrindade@umov.me>
2026-01-13 14:35:46 -0300

    Inicializada aplicação

---
+++ cypress/e2e/describe-it.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+describe('Explorando describe e it', () => {
+
+    it('Valida o título da página', () => {
+        cy.visit('https://antoniotrindade.com.br/treinoautomacao/');
+        cy.title().should('be.equal', 'Treino Automação de Testes');
+
+        cy.get(':nth-child(6) > a').click();
+        cy.get('[name="txtbox1"]').type('Teste de Campo').should('have.value', 'Teste de Campo');
+    });
+
+});
