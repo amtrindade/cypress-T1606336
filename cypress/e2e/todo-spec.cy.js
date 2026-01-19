365d664289d5d7ffcb00dab0a8046bd45c27338a
Antonio Trindade <atrindade@umov.me>
2026-01-19 19:44:52 -0300

    Adiciona testes para marcar, desmarcar e excluir tarefas na lista de tarefas

--- cypress/e2e/todo-spec.cy.js
+++ cypress/e2e/todo-spec.cy.js
@@ -23,7 +23,28 @@ describe('Trabalhando com a lista de tarefas', () => {
         })
 
         it('Marcar tarefa como concluída', () => {
+            cy.get('.todo-list').contains('Estudar Cypress').parent().find('.toggle').check()
 
+            // Clica no filtro "Completed"
+            cy.get('.filters').contains('Completed').click()
+
+            // Valida que apenas a nova tarefa está concluída
+            cy.get('.todo-list li').should('have.length', 1)
+                .and('have.text', 'Estudar Cypress')
+        })
+
+        it('Excluir a nova tarefa e validar que foi excluída', () => {
+            cy.get('.todo-list').contains('Estudar Cypress').parent().find('.toggle').check()
+
+            //Clica no delete da linha da tarefa
+            cy.get('.todo-list').contains('Estudar Cypress').parent()
+                .find('button.destroy').invoke('show').click()
+
+            // Valida que a lista voltou ao tamanho original
+            cy.get('.todo-list li').should('have.length', 2)
+            
+            // Valida que a descrição da tarefa não existe mais
+            cy.get('.todo-list').should('not.contain.text', 'Estudar Cypress')
         })
     })
 })
